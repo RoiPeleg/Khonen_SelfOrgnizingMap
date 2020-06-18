7e6c7e530af25874f62c80f4a35f3fa6260b2424
ROI <roi2307@gmail.com>
2020-06-18 13:09:48 +0300

    fixed updating

--- Khonen.py
+++ Khonen.py
@@ -1,22 +1,31 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import cv2 as cv
 
 
 class Khonen:
     def __init__(self, m: int, n: int, input_size: int, iterations: int):
         self.m = m
         self.n = n
-        self.learning_rate = 0.2
-        self.weights = np.array([np.random.uniform(-2, 2, (m, n)) for i in range(input_size)]).astype(np.float64)
+        self.learning_rate = 0.6
+        self.weights = self.setLine()
         self.iterations = iterations
 
     def getCorr(self):
         return self.weights
 
+    def setLine(self):
+        w = np.zeros((2, 30, 1))
+        w[0] = np.random.uniform(-2, 2, (30, 1))
+        w[1] = np.ones((30, 1))
+        return w
+
+    def setCricle(self):
+        pass
+
     def fit(self, data: np.ndarray):
-        r = 2
-        ws = self.weights.copy()
+        r = 12
         for it in range(self.iterations):
             for x in data:
                 minerror, index = math.inf, (-1, -1)
@@ -24,22 +33,19 @@ class Khonen:
                     for j in range(self.n):
                         error = ((x - self.weights[:, i, j]) ** 2).sum()
                         (minerror, index) = (error, (i, j)) if error < minerror else (minerror, index)
-                w = self.weights[:, min(index[0]-r,0):max(self.m,index[1]+r), min(index[1]-r,0):max(self.n,index[1]+r)]
-                print(np.subtract(w,x).shape)
-                w += self.learning_rate * (np.subtract(w,x.reshape(2,1)))
-            self.learning_rate *= 0.99
-            r = (max(2, r - 1))
+                window = self.weights[:, max(index[0] - r, 0):min(index[0] + r, self.m), max(index[1] - r, 0):min(index[1] + r, self.m)]
+                window += (self.learning_rate*(np.subtract(x.reshape((x.shape[0], 1, self.n)), window)))
+            self.learning_rate *= np.exp(-it/self.iterations)
+            r *= int(np.exp(-it//1000))
 
 
-
-length = np.sqrt(np.random.uniform(0, 2, 1000))
-angle = np.pi * np.random.uniform(0, 2, 1000)
+length = np.sqrt(np.random.uniform(0, 2, 500))
+angle = np.pi * np.random.uniform(0, 2, 500)
 
 x = length * np.cos(angle)
 y = length * np.sin(angle)
 circle = np.vstack((x, y)).T
-k = Khonen(30, 1, 2, 150)
-w = k.getCorr()
+k = Khonen(30, 1, 2, 1000)
 k.fit(circle)
 plt.scatter(x, y, c='r')
 plt.plot(k.getCorr()[0], k.getCorr()[1], marker='o')
