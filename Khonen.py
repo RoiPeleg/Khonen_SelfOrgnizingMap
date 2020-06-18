59c87ff6b5e34d4f759a1293ba1bddc7b58102ff
ROI <roi2307@gmail.com>
2020-06-19 00:49:14 +0300

    small circle

--- Khonen.py
+++ Khonen.py
@@ -6,7 +6,7 @@ class Khonen:
     def __init__(self, m: int, n: int, input_size: int, iterations: int):
         self.m = m
         self.n = n
-        self.learning_rate = 0.6
+        self.learning_rate = 0.3
         self.weights = self.setLine()
         self.iterations = iterations
 
@@ -23,7 +23,7 @@ class Khonen:
         pass
 
     def fit(self, data: np.ndarray):
-        r0 = 10
+        r0 = 4
         r = r0
         lr = self.learning_rate
         for it in range(self.iterations):
@@ -34,9 +34,9 @@ class Khonen:
                         error = ((x - self.weights[:, i, j]) ** 2).sum()
                         (minerror, index) = (error, (i, j)) if error < minerror else (minerror, index)
                 window = self.weights[:, max(index[0] - r, 0):min(index[0] + r, self.m), max(index[1] - r, 0):min(index[1] + r, self.m)]
-                window += (self.learning_rate*(np.subtract(x.reshape((x.shape[0], 1, self.n)), window)))
-            self.learning_rate =lr*np.exp(-it/self.iterations)
-            r = min(int(r0*np.exp(-it//100)),1)
+                window += (self.learning_rate*np.exp(- self.weights[:, index[0], index[1]]/(2*(r**2))).reshape((x.shape[0], 1, self.n))*(np.subtract(x.reshape((x.shape[0], 1, self.n)), window)))
+            self.learning_rate = lr *np.exp(-it/self.iterations)
+            #r = min(int(r0*np.exp(-it//100000)),1)
 
 
 length = np.sqrt(np.random.uniform(0, 2, 800))
@@ -45,7 +45,7 @@ angle = np.pi * np.random.uniform(0, 2, 800)
 x = length * np.cos(angle)
 y = length * np.sin(angle)
 circle = np.vstack((x, y)).T
-k = Khonen(30, 1, 2, 700)
+k = Khonen(30, 1, 2, 1000)
 k.fit(circle)
 plt.scatter(x, y, c='r')
 plt.plot(k.getCorr()[0], k.getCorr()[1], marker='o')
