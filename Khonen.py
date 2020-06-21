8272eb6697a74f1cb152005a8d3ea28b6b8cdd82
ROI <roi2307@gmail.com>
2020-06-22 00:50:54 +0300

    last commit with priority ring example

--- Khonen.py
+++ Khonen.py
@@ -3,27 +3,52 @@ import math
 import matplotlib.pyplot as plt
 
 class Khonen:
+
     def __init__(self, m: int, n: int, input_size: int, iterations: int):
         self.m = m
         self.n = n
-        self.learning_rate = 0.3
+        self.learning_rate = 0.001
         self.weights = self.setLine()
         self.iterations = iterations
 
+
     def getCorr(self):
         return self.weights
-
     def setLine(self):
         w = np.zeros((2, 30, 1))
-        w[0] = np.random.uniform(-2, 2, (30, 1))
-        w[1] = np.ones((30, 1))
+        w[0] = np.random.uniform(-.2, .2, (30, 1))
+        w[1] = np.random.uniform(-.2, .2, (30, 1))
         return w
 
     def setCricle(self):
-        pass
+        w = np.zeros((2, 30, 1))
+        w[0] = np.random.uniform(-.2, .2, (30, 1))
+        w[1] = np.random.uniform(-.2, .2, (30, 1))
+        return w
+
+    def circle_update(self, index, r, x):
+        window = self.weights[:, max(index[0] - r, 0):min(index[0] + r, self.m-1),
+                 max(index[1] - r, 0):min(index[1] + r, self.n)]
+        window += (self.learning_rate * np.exp(- self.weights[:, index[0], index[1]] / (2 * (r ** 2))).reshape(
+            (x.shape[0], 1, 1)) * (np.subtract(x.reshape((x.shape[0], 1, 1)), window)))
+        if index[0] + r >= self.m:
+            window = self.weights[:,  0:((index[0] + r)-self.m)+1, max(index[1] - r, 0):min(index[1] + r, self.n)]
+            window += (self.learning_rate * np.exp(- self.weights[:, index[0], index[1]] / (2 * (r ** 2))).reshape(
+                (x.shape[0], 1, 1)) * (np.subtract(x.reshape((x.shape[0], 1,1)), window)))
+        elif index[0] - r < 0:
+            window = self.weights[:, index[0] - r: , max(index[1] - r, 0):min(index[1] + r, self.n)]
+            window += (self.learning_rate * np.exp(- self.weights[:, index[0], index[1]] / (2 * (r ** 2))).reshape(
+                (x.shape[0], 1, 1)) * (np.subtract(x.reshape((x.shape[0], 1, 1)), window)))
+
+    def square_update(self,index,r,x):
+        window = self.weights[:, max(index[0] - r, 0):min(index[0] + r, self.m),
+                 max(index[1] - r, 0):min(index[1] + r, self.m)]
+        window += (self.learning_rate * np.exp(
+            - self.weights[:, index[0], index[1]] / (2 * (r ** 2))).reshape((x.shape[0], 1, 1)) * (
+                       np.subtract(x.reshape((x.shape[0], 1, 1)), window)))
 
     def fit(self, data: np.ndarray):
-        r0 = 8
+        r0 = 1
         r = r0
         lr = self.learning_rate
         for it in range(self.iterations):
@@ -33,20 +58,18 @@ class Khonen:
                     for j in range(self.n):
                         error = ((x - self.weights[:, i, j]) ** 2).sum()
                         (minerror, index) = (error, (i, j)) if error < minerror else (minerror, index)
-                window = self.weights[:, max(index[0] - r, 0):min(index[0] + r, self.m), max(index[1] - r, 0):min(index[1] + r, self.m)]
-                window += (self.learning_rate*np.exp(- self.weights[:, index[0], index[1]]/(2*(r**2))).reshape((x.shape[0], 1, self.n))*(np.subtract(x.reshape((x.shape[0], 1, self.n)), window)))
-            self.learning_rate = lr *np.exp(-it/self.iterations)
-            r = min(int(r0*np.exp(-it//1000)),3)
+                self.circle_update(index, r, x)
+            self.learning_rate = lr * np.exp(-it / self.iterations)
+            r = max(int(r0*np.exp(-it//100)), 1)
 
 
-length = np.sqrt(np.random.uniform(0, 2, 800))
+length = np.sqrt(np.random.rand(800)*3+1)*2
 angle = np.pi * np.random.uniform(0, 2, 800)
-
 x = length * np.cos(angle)
 y = length * np.sin(angle)
 circle = np.vstack((x, y)).T
-k = Khonen(30, 1, 2, 600)
+k = Khonen(30, 1, 2, 800)
 k.fit(circle)
-plt.scatter(x, y, c='r')
-plt.plot(k.getCorr()[0], k.getCorr()[1], marker='o')
+plt.scatter(x, y, c='b')
+plt.scatter(k.getCorr()[0], k.getCorr()[1], c='r', marker='o')
 plt.show()
